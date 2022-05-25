12c5dd195202f18326f255edb4c925069998ae9e
Alex_Bond <76467364+student-Alex-Bond@users.noreply.github.com>
2022-05-25 22:39:41 +0600

    added webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,20 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
     entry: ["./src/index.js"],
     output: {
         path: __dirname + '/dist',
         filename: "bundle.js"
+    },
+    devServer: {
+        static: __dirname + '/dist',
+        port: 3000,
+    },
+    plugins: [new HtmlWebpackPlugin({
+        filename: "index.html",
+        template: "./src/index.html"
+    })],
+    resolve: {
+        extensions: ['.js']
     }
 }
