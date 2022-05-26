ca54ea873a649453fd8daf275ec9901d1fac8cb9
Alex_Bond <76467364+student-Alex-Bond@users.noreply.github.com>
2022-05-26 11:32:35 +0600

    added method render all posts

--- src/components/posts.component.js
+++ src/components/posts.component.js
@@ -1,6 +1,7 @@
 import {Component} from '../core/component'
 import {apiService} from '../services/api.service'
 import {TransformService} from '../services/transform.service'
+
 export class PostsComponent extends Component {
 	constructor(id) {
 		super(id);
@@ -8,8 +9,36 @@ export class PostsComponent extends Component {
 
 	async onShow() {
 		const fbData = await apiService.asyncFetchPosts()
-		const allPosts = 	TransformService.fbObjectToArray(fbData)
-		console.log(allPosts)
+		const allPosts = TransformService.fbObjectToArray(fbData)
+		const html = allPosts.map(post => renderPost(post)).join(' ')
+		this.$el.insertAdjacentHTML("afterbegin", html)
 	}
+	onHide() {
+		this.$el.innerHTML =''
+	}
+}
+
+function renderPost(post) {
+	const tag = post.type === 'news'
+		? '<li class="tag tag-blue tag-rounded">Новость</li>'
+		: '<li class="tag tag-green tag-rounded">Заметка</li>'
+	const button = '<button class="button-round button-small button-secondary">Save</button>'
+	return `
+	  <div class="panel">
+      <div class="panel-head">
+        <p class="panel-title">${post.title}</p>
+        <ul class="tags">
+          ${tag}
+        </ul>
+      </div>
+      <div class="panel-body">
+        <p class="multi-line">${post.fulltext}</p>
+      </div>
+      <div class="panel-footer w-panel-footer">
+        <small>${post.date}</small>
+        ${button}
+      </div>
+    </div>
+	`
 }
 
